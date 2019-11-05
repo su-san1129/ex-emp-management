d759e27fc8ff033dbd31a47661c4799e7e06d2ae
su-san1129 <deviationvalue1023@outlook.jp>
2019-11-05 17:46:46 +0900

    [fix]Employee Repository

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -4,17 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import jp.co.sample.domain.Employee;
 
+/**
+ * @author takahiro.suzuki
+ *
+ */
 public class EmployeeRepository {
 	
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
+	/**
+	 * Employeeオブジェクトに情報を挿入するマッパー
+	 */
 	private final RowMapper<Employee> Employee_ROW_MAPPER = (rs, i) -> {
 		Employee employee = new Employee();
 		employee.setId(rs.getInt("id"));
@@ -32,6 +40,10 @@ public class EmployeeRepository {
 		return employee;
 	};
 	
+	/**
+	 * 全件検索
+	 * @return employeeList
+	 */
 	public List<Employee> findAll(){
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT id, name, image, gender, hire_date, mail_address, zip_code, address, telephone, salary, characteristics, dependentsCount ");
@@ -42,6 +54,11 @@ public class EmployeeRepository {
 		return employeeList;
 	}
 	
+	/**
+	 * 主キーから従業員情報を取得する
+	 * @param id
+	 * @return Employee
+	 */
 	public Employee load(Integer id) {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT id, name, image, gender, hire_date, mail_address, zip_code, address, telephone, salary, characteristics, dependentsCount ");
@@ -51,6 +68,16 @@ public class EmployeeRepository {
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 		Employee employee = template.queryForObject(paramsql, param, Employee_ROW_MAPPER);
 		return employee;
+	}
+	
+	/**
+	 * 従業員情報を変更する(扶養人数)
+	 * @param employee
+	 */
+	public void update(Employee employee) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(employee);
+		String updateSql = "UPDATE employees SET dependentsCount = :dependentCount WHERE id = :id;";
+		template.update(updateSql, params);
 	}
 
 }
